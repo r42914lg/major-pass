bd929a3f73683f239f3d1fb9b72e11031a7f7917
Leonid Gomberg <r42914lg@gmail.com>
2025-12-17 10:50:21 +0300

    add swipe to delete

--- app/src/main/java/com/r42914lg/major/MainActivity.kt
+++ app/src/main/java/com/r42914lg/major/MainActivity.kt
@@ -11,8 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.r42914lg.major.model.Car
+import com.r42914lg.major.model.Visitor
 import com.r42914lg.major.ui.Visitors
 import com.r42914lg.major.ui.theme.MajorTheme
+import kotlin.collections.listOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +25,28 @@ class MainActivity : ComponentActivity() {
             MajorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Visitors(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        visitors = listOf(
+                                Visitor(
+                                    "Кац",
+                                    listOf(
+                                        Car("Мерседес", "х156уя197"),
+                                        Car("Ауди", "е996ен150"),
+                                    )
+                                ),
+                            Visitor(
+                                "Мойша",
+                                listOf(
+                                    Car("Жигули", "м0248мм"),
+                                )
+                            ),
+                            Visitor(
+                                "Бегемот",
+                                listOf(
+                                    Car("Форд", "к7208мо"),
+                                )
+                            ),
+                        )
                     )
                 }
             }
--- app/src/main/java/com/r42914lg/major/ui/listing.kt
+++ app/src/main/java/com/r42914lg/major/ui/listing.kt
@@ -1,14 +1,27 @@
 package com.r42914lg.major.ui
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,6 +29,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.r42914lg.major.model.Car
@@ -25,54 +40,101 @@ import com.r42914lg.major.ui.theme.MajorTheme
 @Composable
 fun Visitors(
     modifier: Modifier = Modifier,
-    visitors: List<Visitor> = emptyList()
+    visitors: List<Visitor> = emptyList(),
+    onDelete: (Visitor) -> Unit = {}
 ) {
     LazyColumn(modifier) {
-        visitors.forEach {
-            item {
-                VisitorCard(visitor = it)
-            }
+        items(visitors, key = { it.name }) { visitor ->
+            VisitorCard(visitor = visitor, onDelete = onDelete)
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VisitorCard(
     modifier: Modifier = Modifier,
     visitor: Visitor = Visitor(),
+    onDelete: (Visitor) -> Unit = {}
 ) {
-    Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(text = visitor.name)
-            Column(
-                modifier = Modifier.padding(top = 8.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+    val dismissState = rememberSwipeToDismissBoxState(
+        confirmValueChange = {
+            if (it == SwipeToDismissBoxValue.EndToStart) {
+                onDelete(visitor)
+                true
+            } else {
+                false
+            }
+        },
+    )
+    SwipeToDismissBox(
+        state = dismissState,
+        modifier = modifier.padding(vertical = 4.dp),
+        enableDismissFromEndToStart = true,
+        enableDismissFromStartToEnd = false,
+        backgroundContent = {
+            val color by animateColorAsState(
+                when (dismissState.targetValue) {
+                    SwipeToDismissBoxValue.Settled -> Color.LightGray
+                    SwipeToDismissBoxValue.EndToStart -> Color.Red
+                    else -> Color.Transparent
+                },
+                label = "background color"
+            )
+            val alignment = Alignment.CenterEnd
+            val icon = Icons.Default.Delete
+            val scale by animateFloatAsState(
+                if (dismissState.targetValue == SwipeToDismissBoxValue.Settled) 0.75f else 1f,
+                label = "icon scale"
+            )
+
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(color)
+                    .padding(horizontal = 20.dp),
+                contentAlignment = alignment
             ) {
-                visitor.cars.forEach { car ->
-                    var isChecked by remember { mutableStateOf(false) }
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Checkbox(
-                            checked = isChecked,
-                            onCheckedChange = { isChecked = it }
-                        )
-                        Text(
-                            text = car.make,
-                            modifier = Modifier.padding(start = 8.dp)
-                        )
+                Icon(
+                    icon,
+                    contentDescription = "Delete",
+                    modifier = Modifier.scale(scale)
+                )
+            }
+        }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = visitor.name)
+                Column(
+                    modifier = Modifier.padding(top = 8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    visitor.cars.forEach { car ->
+                        var isChecked by remember { mutableStateOf(false) }
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Checkbox(
+                                checked = isChecked,
+                                onCheckedChange = { isChecked = it }
+                            )
+                            Text(
+                                text = car.make,
+                                modifier = Modifier.padding(start = 8.dp)
+                            )
+                        }
                     }
                 }
             }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
